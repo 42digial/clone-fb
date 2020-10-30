32abcca34126b18f8324863e5669ffc41026e50b
Edvan <eadelima@outlook.com>
2020-10-29 23:43:10 -0300

    feat: 🎸 initialize commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="app__body">
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widget */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSS0wlCPA8-rctjQU33m9TXZnkJyL2dNk",
+  authDomain: "clone-facebook-a490b.firebaseapp.com",
+  databaseURL: "https://clone-facebook-a490b.firebaseio.com",
+  projectId: "clone-facebook-a490b",
+  storageBucket: "clone-facebook-a490b.appspot.com",
+  messagingSenderId: "774384406332",
+  appId: "1:774384406332:web:712e40942c654870e3d839"
+};
